f843caac6ac60762d612fd7a3a7d2175682503af
ann.kurenna <anna.kurennaya38@gmail.com>
2020-01-25 19:42:54 +0200

    V2

---
+++ Platformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigidBody.AddForce(0, 0, 200*Time.deltaTime);
+    }
+}
