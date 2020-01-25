31da246c66777343080c4fe43156f04c8ffc185f
ann.kurenna <anna.kurennaya38@gmail.com>
2020-01-25 20:32:52 +0200

    V4 camera follow

---
+++ Platformer/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] Transform PlayerPos;
+    [SerializeField] Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = PlayerPos.position + offset;
+    }
+}
