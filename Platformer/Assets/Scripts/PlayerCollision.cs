4ff6ae34d852c4df9088177de161adf4fab28a60
ann.kurenna <anna.kurennaya38@gmail.com>
2020-01-25 20:55:34 +0200

    V5 collision

---
+++ Platformer/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    [SerializeField] PlayerMovement movement;
+
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+            movement.enabled = false;
+    }
+}
