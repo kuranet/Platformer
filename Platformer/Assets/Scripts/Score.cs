4aeb62acc12eb0ff2264e513c131ffb4cf2b3765
ann.kurenna <anna.kurennaya38@gmail.com>
2020-01-26 12:27:50 +0200

    V7 score

---
+++ Platformer/Assets/Scripts/Score.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] Text scoreText;
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = (player.position.z/10).ToString("0");
+    }
+}
